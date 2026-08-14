126e4536e89b42f2c0d839877d424ab4c1b34520
fgfor22-lab <fgfor22@gmail.com>
2026-08-14 21:06:36 +0300

    Update channel identifier in bidding_bot.dart

--- bidding_bot.dart
+++ bidding_bot.dart
@@ -15,7 +15,7 @@ List<int> bannedUsers = [];
 const String dbUrl = 'https://warshti-9911e-default-rtdb.firebaseio.com/';
 
 // 🔴 ضع مُعرّف قناتك هنا (مع علامة @) 🔴
-const String myChannel = '@warshti_iq'; 
+const String myChannel = '@warshtii'; 
 
 void saveUserToFirebase(int chatId) async {
   try {
